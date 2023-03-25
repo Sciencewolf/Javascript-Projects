5ed00af8b98db0f860a731c1cbbdf0aeaeb7bec9
Sciencewolf <aron17marton@gmail.com>
2023-03-25 23:59:35 +0100

    Initial commit

--- todoapp/main.js
+++ todoapp/main.js
@@ -1,12 +1,14 @@
 const onLoad = () => {
-    console.log("!")
+    if(getPlatform() === "mobile") mobileVersion()
+    else if(getPlatform() === "desktop") desktopVersion()
+    quotes()
 }
 
 function addTask() {
 }
 
 function removeTask() {
-
+    
 }
 
 function cookies() {
@@ -14,13 +16,35 @@ function cookies() {
 }
 
 function dropdown() {
-
+    
 }
 
 async function quotes() {
+    const tip_div = document.querySelector('.quote')
+    let rand_num = 0
+    rand_num = Math.floor(Math.random() * 25)
+    console.log(rand_num)
 
+    let data = await fetch("https://raw.githubusercontent.com/Sciencewolf/Javascript-Projects/master/todoapp/quotes.json")
+    let json = await data.json();
+    let str = "Quote of the Day: ".concat(JSON.stringify(json["quotes"][`${rand_num}`][`${++rand_num}`]))
+
+    tip_div.innerHTML = str.replaceAll('"', '')
 }
 
 function footer() {
+
+}
+
+function getPlatform() {
+    const isMobile = /android|iphone|ipad|ipod/i.test(navigator.userAgent)
+    return isMobile ? "mobile" : "desktop"
+}
+
+function mobileVersion() {
+
+}
+
+function desktopVersion() {
 
 }
