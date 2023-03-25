d822e629fa3d0f0f792994a5086c7274754fc6cb
Sciencewolf <aron17marton@gmail.com>
2023-03-25 23:17:00 +0100

    first structure of the website

---
+++ todoapp/main.js
@@ -0,0 +1,26 @@
+const onLoad = () => {
+    console.log("!")
+}
+
+function addTask() {
+}
+
+function removeTask() {
+
+}
+
+function cookies() {
+
+}
+
+function dropdown() {
+
+}
+
+async function quotes() {
+
+}
+
+function footer() {
+
+}
