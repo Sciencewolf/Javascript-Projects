211f777e1f7cd2942cdde54d8fae54d4ed8545ee
Sciencewolf <aron17marton@gmail.com>
2023-03-29 21:16:17 +0200

    Need to fix issue with remove and comp buttons

--- todoapp/main.js
+++ todoapp/main.js
@@ -1,46 +1,62 @@
 function onLoad() {
     if(getPlatform() === "mobile") mobileVersion()
     else if(getPlatform() === "desktop") desktopVersion()
-    quotes()
     actions()
 }
 
 function actions() {
-    const [addtask_fn] = addTask()
-    addtask_fn()
-    removeTask()
-    dropdown()
+    addTask()
     footer()
 }
 
+function setInputToToday() {
+    const date = new Date()
+    let _date = date.toISOString().substring(0, 10)
+    return _date
+}
+
 function addTask() {
+    const list_todo_div = document.querySelector('.list-todo')
+    const add_button = document.getElementById("btn-add")
+    const list = document.querySelector('.list')
+    const select_ = document.querySelector('.dropdown')
+    const input_date = document.getElementById('input-date')
+    const input_value = document.getElementById('input-task')
 
-    const map_of_tasks = new Map() // to store tasks, -- for delete and complete tasks
-    let count = 0
-
-    function create_div() {
-        const list_todo_div = document.querySelector('.list-todo')
-        const add_button = document.getElementById("btn-add")
-        const list = document.querySelector('.list')
-        const select_ = document.querySelector('.dropdown')
-
-        add_button.addEventListener('click', () => {
-            const input_value = document.getElementById('input-task')
-
+    add_button.addEventListener('click', () => {
+        if(input_value.value === "") {
+            input_value.style.border = "1px solid red"
+        }
+        else {
+            input_value.style.border = "none"
             const span = document.createElement('span')
             span.className = "span-task"
 
-            const p = document.createElement('p')
-            p.id = "p-task"
-            p.innerHTML = input_value.value
+            const p_task = document.createElement('p')
+            p_task.id = "p-task"
+            p_task.innerHTML = input_value.value
 
             const p_type = document.createElement('p')
             p_type.id = "p-type"
-            p_type.innerHTML = select_.options[select_.selectedIndex].text
+            if (select_.options[select_.selectedIndex].text === "-- Select --") {
+                p_type.innerText = "Other"
+            }
+            else {
+                p_type.innerText = select_.options[select_.selectedIndex].text
+            }
+
+            const p_date = document.createElement('p')
+            p_date.id = "p-date"
+            if(input_date.value === "") {
+                p_date.innerHTML = setInputToToday()
+            }
+            else p_date.innerText = input_date.value
 
             const remove_button = document.createElement('button')
             remove_button.className = "btn-remove"
-            remove_button.innerText = "Delete"
+            remove_button.innerText = "Remove"
+            // remove_button.addEventListener('click', removeTask()) // 
+
             const i_ = document.createElement('i')
             i_.className = "fa fa-close"
             remove_button.appendChild(i_)
@@ -48,55 +64,60 @@ function addTask() {
             const complete_button = document.createElement('button')
             complete_button.className = "btn-complete"
             complete_button.innerText = "Complete"
+            // completeTask.addEventListener('click', completeTask()) // 
+
             const i__ = document.createElement('i')
             i__.className = "fa-solid fa-check"
             complete_button.appendChild(i__)
 
-            span.appendChild(p)
+            span.appendChild(p_task)
             span.appendChild(p_type)
+            span.appendChild(p_date)
             span.appendChild(remove_button)
             span.appendChild(complete_button)
 
             list.appendChild(span)
             list_todo_div.style.display = "block"
 
-            map_of_tasks.set(++count, list)
-            // console.log(map_of_tasks.entries())
-        })
-    }
-    return [create_div, map_of_tasks]
-}
-
-function removeTask() {
-    
-}
-
-function cookies() {
-
-}
-
-function dropdown() {
-    const dropdown_div = document.querySelector('.dropdown')
-
-    dropdown_div.addEventListener('click', () => {
-        
+            // reset all inputs to default
+            input_value.value = ""
+            select_.selectedIndex = 0
+            input_date.value = ""
+        }
     })
 }
 
+function removeTask() {
+    const remove_btn = document.querySelector('.btn-remove')
+    const list = document.querySelector('.list')
+
+    let parent_span_of_removebtn = remove_btn.parentElement
+    list.removeChild(parent_span_of_removebtn)
+}
+
+function completeTask() {
+    const complete_btn = document.querySelector('.btn-complete')
+    let parent_span_of_completebtn = complete_btn.parentElement
+    parent_span_of_completebtn.style.backgroundColor = "lightslategrey"
+    complete_btn.style.curson = "default"
+}
+
 async function quotes() {
-    const tip_div = document.querySelector('.quote')
-    let rand_num = 0
-    rand_num = Math.floor(Math.random() * 25)
+    const quote_div = document.querySelector('.quote')
+    let rand_num = Math.floor(Math.random() * 25)
 
     let data = await fetch("https://raw.githubusercontent.com/Sciencewolf/Javascript-Projects/master/todoapp/quotes.json")
     let json = await data.json();
-    let str = "Quote of the Day: ".concat(JSON.stringify(json["quotes"][`${rand_num}`][`${++rand_num}`]))
+    let str = `<span id='span-qouteoftheday'>Quote of the Day:</span>   ${JSON.stringify(json["quotes"][`${rand_num}`][`${++rand_num}`]) }`
 
-    tip_div.innerHTML = str.replaceAll('"', '')
+    let wordsIn_str = str.split(" ")
+    let count_words = (wordsIn_str.length - 4) * 700
+
+    quote_div.innerHTML = str.replaceAll('"', '')
 
     setTimeout(() => {
-        tip_div.style.display = "none"
-    }, 2000)
+        quote_div.style.display = "none"
+    }, count_words)
 }
 
 function footer() {
@@ -104,7 +125,7 @@ function footer() {
     const date = new Date()
     
     dev.innerHTML = `Created By: Aron Marton \
-    <a href="https://github.com/Sciencewolf/Javascript-Projects/tree/master/todoapp" target="_blank">[Source Code]</a> ${date.getFullYear()}`
+    [<a href="https://github.com/Sciencewolf" target="_blank">GitHub</a>] ${date.getFullYear()}`
 }
 
 function getPlatform() {
@@ -112,10 +133,31 @@ function getPlatform() {
     return isMobile ? "mobile" : "desktop"
 }
 
-function mobileVersion() {
+async function mobileVersion() {
 
+    async function quote() {
+        const quote_div = document.querySelector('.quote')
+        let rand_num = Math.floor(Math.random() * 10)
+
+        let data = await fetch("https://raw.githubusercontent.com/Sciencewolf/Javascript-Projects/master/todoapp/short-quotes.json")
+        let json = await data.json();
+        let quote_from_json = JSON.stringify(json["quotes"][`${rand_num}`][`${++rand_num}`])
+        let words_len = (quote_from_json.split(' ').length)
+
+        let str = `<span id='span-qouteoftheday'>Quote of the Day:</span> ${quote_from_json}`
+        quote_div.innerHTML = str.replaceAll('"', '')
+
+        setTimeout(() => {
+            quote_div.style.display = "none"
+        }, words_len * 900)
+    }
+    quote()
 }
 
 function desktopVersion() {
+    quotes()
+}
+
+function cookies() {
 
 }
