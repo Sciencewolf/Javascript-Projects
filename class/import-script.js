495e6e493750e59428a7dbdbb9ce8345c68db6a7
Sciencewolf <aron17marton@gmail.com>
2023-04-01 22:44:18 +0200

    Added new items

--- class/import-script.js
+++ class/import-script.js
@@ -6,8 +6,37 @@ class Import {
         dev.innerHTML = `Created By: Aron Marton \
         [<a href="https://github.com/Sciencewolf" target="_blank">GitHub</a>] ${date.getFullYear()}`
     }
+
+    // Maintenance for glitch 
+    showMaintenanceWindow() {
+        const html = document.querySelector('html')
+        const body = document.querySelector('body')
+
+        const body_new = document.createElement('body')
+        const div = document.createElement('div')
+        const img = document.createElement('img')
+        const span_new = document.createElement('span')
+
+        body_new.className = "body-maintenance"
+        div.innerHTML = "Maintenance"
+        div.className = "div-maintenance"
+        img.src = "https://img.icons8.com/ios/100/null/online-maintenance-portal.png"
+        img.id = "img-maintenance"
+        span_new.className = "span-return_later"
+        span_new.innerHTML = 'Come Back Later'
+
+        body.innerHTML = ""
+        body.style.width = 0
+        body.style.height = 0
+
+        body_new.appendChild(div)
+        body_new.appendChild(img)
+        body_new.appendChild(span_new)
+        html.appendChild(body_new)
+    }
 }
 
 const _Import = new Import()
 
 _Import.footer()
+_Import.showMaintenanceWindow()
