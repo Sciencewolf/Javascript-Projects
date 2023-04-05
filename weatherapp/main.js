4c2f8349435651868ab576c076715964361687c3
Sciencewolf <aron17marton@gmail.com>
2023-04-05 02:28:53 +0200

    Initial commit

--- weatherapp/main.js
+++ weatherapp/main.js
@@ -3,6 +3,7 @@ class objects {
         this.x = x
         this.y = y
     }
+
     static objWeathercode = {
         0: "Clear",
         1: "Mainly Clear",
@@ -33,30 +34,44 @@ class objects {
         96: "Thunderstorm: with Slight and Heavy Hail"
     }
 
+    static images = {
+        "sunset": "<img src='https://img.icons8.com/ios/50/null/sunset--v1.png'/>",
+        "sunrise": "<img src='https://img.icons8.com/ios/50/null/sunrise--v1.png'/>",
+        "precipitation": "<img src='https://img.icons8.com/external-microdots-premium-microdot-graphic/64/null/external-precipitation-weather-forecast-microdots-premium-microdot-graphic-2.png' />",
+        "visibility": '<img src="https://img.icons8.com/fluency-systems-filled/48/null/visible.png"/>',
+        "humidity": '<img src="https://img.icons8.com/ios-glyphs/30/null/hygrometer.png"/>',
+    }
+
     static compassDirections = [
         "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"
     ]
 
     static url_daily_temp_max_min(class_) {
-        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&daily=temperature_2m_max,temperature_2m_min`
+        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&daily=temperature_2m_max,temperature_2m_min&forecast_days=1`
     }
+
     static url_daily_sunrise_sunset(class_) {
-        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&daily=sunrise,sunset`
+        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&daily=sunrise,sunset&forecast_days=1`
     }
+
     static url_daily_rain(class_) {
-        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&daily=precipitation_probability_mean`
+        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&daily=precipitation_probability_mean&forecast_days=1`
     }
+
     static url_daily_currentweather(class_) {
-        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&current_weather=true`
+        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&current_weather=true&forecast_days=1`
     }
+
     static url_hourly_visibility(class_) {
-        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&hourly=visibility`
+        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&hourly=visibility&forecast_days=1`
     }
+
     static url_hourly_humidity(class_) {
-        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&hourly=relativehumidity_2m`
+        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&hourly=relativehumidity_2m&forecast_days=1`
     }
+
     static url_hourly_weathercode(class_) {
-        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&hourly=weathercode`
+        return `https://api.open-meteo.com/v1/forecast?latitude=${class_.x}&longitude=${class_.y}&timezone=Europe/Budapest&hourly=weathercode&forecast_days=1`
     }
 }
 
@@ -86,9 +101,9 @@ async function position(pos) {
         response => {
             return response.json()
         }
-    ).then(resJSON => 
+    ).then(resJSON =>
         span_geolocation.innerHTML = `${resJSON['city']},  ${resJSON['countryCode']}`
-    ).catch(err => {
+    ).catch(_ => {
         span_geolocation.innerHTML = "Error!"
     })
     await fetchDataFromWeatherAPI(x, y)
@@ -99,14 +114,14 @@ async function fetchDataFromWeatherAPI(x, y) {
 
     let json = ""
     let json_time = ""
-    let urltime = "http://worldtimeapi.org/api/timezone/Europe/Budapest"
+    let urltime = "https://worldtimeapi.org/api/timezone/Europe/Budapest"
 
     async function fetchData(_url) {
         try {
             const response = await fetch(_url, { method: "GET" })
             return json = await response.json()
         }catch (err) {
-            console.log(err);
+            console.log(err)
         }
     }
 
@@ -115,29 +130,29 @@ async function fetchDataFromWeatherAPI(x, y) {
             const response = await fetch(__url)
             return json_time = await response.json()
         }catch(err) {
-            console.log(err);
+            console.log(err)
         }
     }
 
-    async function fetchDataDaily(day) {
+    async function fetchDataDaily() {
         await fetchData(objects.url_daily_temp_max_min(objClass))
         return [
-            "H: " + Math.round(json["daily"]["temperature_2m_max"][day]) + "° ",
-            "L: " + Math.round(json["daily"]["temperature_2m_min"][day]) + "° "
+            "H: " + Math.round(json["daily"]["temperature_2m_max"][0]) + "° ",
+            "L: " + Math.round(json["daily"]["temperature_2m_min"][0]) + "° "
         ]
     }
 
-    async function fetchDataSunriseSunset(day) {
+    async function fetchDataSunriseSunset() {
         await fetchData(objects.url_daily_sunrise_sunset(objClass))
         return [
-            "Sunrise: " + json['daily']["sunrise"][day].slice(11) + "<br>",
-            "Sunset: " + json['daily']['sunset'][day].slice(11)
+            json['daily']["sunrise"][0].slice(11) + "<br>",
+            json['daily']['sunset'][0].slice(11)
         ]
     }
 
-    async function fetchDataRain(day) {
+    async function fetchDataRain() {
         await fetchData(objects.url_daily_rain(objClass))
-        return "Precipitation: " + json['daily']['precipitation_probability_mean'][day] + " %"
+        return json['daily']['precipitation_probability_mean'][0] + " %"
     }
 
     async function fetchDataCurrentWeather() {
@@ -145,15 +160,14 @@ async function fetchDataFromWeatherAPI(x, y) {
         return json['current_weather']
     }
 
-    async function fetchDataVisibility(hour) { // now time hour
+    async function fetchDataVisibility(hour) {
         await fetchData(objects.url_hourly_visibility(objClass))
-        console.log(json)
-        return "Visibility: " + json['hourly']['visibility'][hour] + " m"
+        return json['hourly']['visibility'][hour] + " m"
     }
 
-    async function fetchDataHumidity(hour) { // need to get now time hour
+    async function fetchDataHumidity(hour) {
         await fetchData(objects.url_hourly_humidity(objClass))
-        return "Humidity: " + json['hourly']['relativehumidity_2m'][hour] + " %"
+        return json['hourly']['relativehumidity_2m'][hour] + " %"
     }
 
     async function fetchDataWeathercode() {
@@ -168,9 +182,9 @@ async function fetchDataFromWeatherAPI(x, y) {
         await fetchTime(urltime)
         return json_time['datetime']
     }
+
     // Paste Data
     async function pasteData() {
-
         // main divs
         const hl_temperature_div = document.querySelector('.hl-temperature')
         const srsn_sun_div = document.querySelector('.srsn-sun')
@@ -181,7 +195,7 @@ async function fetchDataFromWeatherAPI(x, y) {
         const wait_span = document.querySelector('.wait')
 
         // current weather divs
-        const current_weather_div = document.querySelector('.current-weather')
+        const current_weather_div = document.querySelector('.wrapper-current-weather')
         const current_temperature_div = document.querySelector('.current-temperature')
         const current_windspeed_div = document.querySelector('.current-windspeed')
         const current_winddirection_div = document.querySelector('.current-winddirection')
@@ -193,33 +207,29 @@ async function fetchDataFromWeatherAPI(x, y) {
         let [sunrise, sunset] = await fetchDataSunriseSunset(0)
         let percentage_of_rain = await fetchDataRain(0)
         let current_weather = await fetchDataCurrentWeather()
-        let visibility = await fetchDataVisibility(5)
-        let humidity = await fetchDataHumidity(16)
         let [time_, weathercode] = await fetchDataWeathercode()
         let _timeCurrent = await fetchTimeCurrentTime()
 
-        let _timecurrent = _timeCurrent.slice(0, -22)
         let currentHour = _timeCurrent.slice(11, -19)
-        let currentDay = []
-        let cu
-
-        // reformat
-        for(let i = 0;i<time_.length;i++) { currentDay[i] = time_[i].slice(0, -6) }
-        let posAt = currentDay.indexOf(_timecurrent)
+        let currentHourlist = []
+        // reformat weathercode
+        for(let i = 0;i<time_.length;i++) { currentHourlist[i] = time_[i].slice(11, -3) }
+        let posAt = currentHourlist.indexOf(currentHour)
         let weathercodeToday = weathercode[posAt]
-
-        let posAtVisibility =
-
+        // reformat visibility
+        let visibility = await fetchDataVisibility(posAt)
+        // reformat humidity
+        let humidity = await fetchDataHumidity(posAt)
 
         // appending data into main divs
         wait_span.innerHTML = ""
         hl_temperature_div.innerHTML = h + l
-        srsn_sun_div.innerHTML = sunrise + sunset
-        rain_chance_div.innerHTML = percentage_of_rain
-        visibility_div.innerHTML = visibility
-        humidity_div.innerHTML = humidity
+        srsn_sun_div.innerHTML = objects.images['sunrise'] + sunrise + objects.images['sunset'] + sunset
+        rain_chance_div.innerHTML = objects.images['precipitation'] + percentage_of_rain
+        visibility_div.innerHTML = objects.images['visibility'] + visibility
+        humidity_div.innerHTML = objects.images['humidity'] + humidity
         weathercode_div.innerHTML = objects.objWeathercode[weathercodeToday]
-        current_weather_div.removeChild(wait_currentweather_div)
+        wait_currentweather_div.innerHTML = "Current Weather: <br>"
 
         let compassDir = Math.round((current_weather['winddirection']) / 45)
 
